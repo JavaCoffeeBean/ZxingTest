0dfcbc17fe4814af43bd5e7ffd131552d1970c83
JavaCoffeeBean <JavaCoffeeBean@gmail.com>
2019-12-27 15:35:21 -0600

    corrected xml layout

--- app/src/main/java/com/example/android/zxingtest/MainActivity.java
+++ app/src/main/java/com/example/android/zxingtest/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.android.zxingtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ZXingScannerView scannerView;
+    private TextView txtResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //init
+        scannerView = (ZXingScannerView) findViewById(R.id.zxscan);
+        txtResult = (TextView) findViewById(R.id.txt_result);
     }
 }
